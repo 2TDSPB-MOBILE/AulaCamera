2ed7b3440393a8799d0149ec6ec2e24a6ea45960
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-10-31 11:43:13 -0300

    Desenvolvido uso da câmera, botões para alternar cameras, ligar flash e botão para compartilhar a foto tirada(lib expo-sharing)

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button,Alert } from 'react-native';
-import { useState,useEffect,useRef } from 'react';
+import { StyleSheet, Text, View, Button, Alert,Image } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import * as Sharing from 'expo-sharing';
 
 //Importe dos components de camera
-import { CameraView,useCameraPermissions } from 'expo-camera';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
 //Biblioteca para salvar a foto na galeria do dispositivo
 import * as MediaLibrary from "expo-media-library"
@@ -12,18 +13,24 @@ import * as MediaLibrary from "expo-media-library"
 
 export default function App() {
   //Estado de permissão da camera
-  const[permissaoCam,requestPermissaoCam] = useCameraPermissions()
+  const [permissaoCam, requestPermissaoCam] = useCameraPermissions()
 
   //Estado de permissão da biblioteca de imagens
-  const[permissaoMedia,requestPermissaoMedia]= MediaLibrary.usePermissions()
+  const [permissaoMedia, requestPermissaoMedia] = MediaLibrary.usePermissions()
 
   //Refêrencia da câmera
   const cameraRef = useRef(null)
 
   //Estado da foto capturada
-  const[foto,setFoto] = useState(null)
+  const [foto, setFoto] = useState(null)
 
-   //Solicitar permissão para acessar a galeria no inicio do app
+  //Estado para alternar entre a câmera front e traseira
+  const[isFrontCamera,setIsFrontCamera]=useState(false)
+
+  //Estado para gerenciamento do flash(câmera)
+  const[flashLigado,setFlashLigado]=useState(false)
+
+  //Solicitar permissão para acessar a galeria no inicio do app
   useEffect(() => {
     if (permissaoMedia === null) return;
     if (!permissaoMedia?.granted) {
@@ -31,9 +38,9 @@ export default function App() {
     }
   }, [])
 
-    
 
-   //Função para tirar foto
+
+  //Função para tirar foto
   const tirarFoto = async () => {
     if (cameraRef.current) {
       const dadoFoto = await cameraRef.current.takePictureAsync(); //captura foto
@@ -50,7 +57,26 @@ export default function App() {
       Alert.alert("Error", "Error ao Salvar Foto.")
     }
   }
- 
+
+  //Função para compartilhar a foto tirada
+  const compartilharFoto = async ()=>{
+    if(foto?.uri && await Sharing.isAvailableAsync()){
+      await Sharing.shareAsync(foto.uri)//Passa a foto para compartilhamento
+    }else{
+      Alert.alert("Erro","Não foi possível compartilhar a foto.")
+    }
+  }
+
+  //Função para alternar entre as câmeras
+  const toggleCameraType = () =>{
+    setIsFrontCamera(!isFrontCamera)
+  }
+
+  //Função para alternar o flash(on/off)
+  const alternarFlash = ()=>{
+    setFlashLigado(!flashLigado)
+  }
+
   //Enquanto a permissão não estiver carregada
   if (!permissaoCam) return <View />
 
@@ -66,12 +92,30 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      
-     <Text>Teste</Text>
-       <CameraView 
+      {!foto ? (
+        <>
+        <CameraView
           ref={cameraRef}
           style={styles.camera}
+          facing={isFrontCamera?'front':'back'}
+          flash={flashLigado?"on":"off"}
         />
+        <Button title='Tirar Foto' onPress={tirarFoto} />
+        <Button title='Alternar Câmera' onPress={toggleCameraType}/>
+        <Button 
+            title={flashLigado?"Desligar Flash":"Ligar Flash"}
+            onPress={alternarFlash}
+        />
+        </>
+      ):(
+        <>
+          <Image source={{uri:foto.uri}} style={styles.preview}/>
+          <Button title='Salvar Foto' onPress={salvarFoto}/>
+          <Button title='Tirar outra foto' onPress={()=>setFoto(null)}/>  
+          <Button title="Compartilhar foto" onPress={compartilharFoto} />
+        </>
+      )}
+
     </View>
   );
 }
@@ -81,5 +125,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  camera:{flex:1}
+  camera: { flex: 1 },
+  preview:{
+    flex:1,
+    resizeMode:"cover"
+  }
 });
